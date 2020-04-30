9dff0dedd11c40bd1441e760395997f3c351a11e
BureikuT <bureikut@hotmail.com>
2020-04-30 13:15:19 -0600

    testing

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "493363548853-rcmiaero5efrs65vrhi2bcj4o4qu4bin.apps.googleusercontent.com",
+  googleClientSecret: "9bn1uGtZ-RKJEnLPrHj4Ffcx",
+  mongoURI:
+    "mongodb+srv://owner:rAu4zOeCjjv2Z5hQ@cluster0-j3lhp.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "bfoyi8gafuibahfvbuayhf;avofdhg32b239g9ahgaipgfhuaibgeragba8y9",
+};
