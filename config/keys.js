1394cc886ec09eb6dee74f7b49b33fb9527048fa
BureikuT <bureikut@hotmail.com>
2020-05-01 14:36:33 -0600

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-  googleClientID:
-    "493363548853-rcmiaero5efrs65vrhi2bcj4o4qu4bin.apps.googleusercontent.com",
-  googleClientSecret: "9bn1uGtZ-RKJEnLPrHj4Ffcx",
-  mongoURI:
-    "mongodb+srv://owner:rAu4zOeCjjv2Z5hQ@cluster0-j3lhp.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "bfoyi8gafuibahfvbuayhf;avofdhg32b239g9ahgaipgfhuaibgeragba8y9",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
