ae6010d735235fab46ffc25b76751c410c81e4c5
BureikuT <bureikut@hotmail.com>
2020-05-14 13:24:27 -0600

    installed redux-form and have the survey set w/ inputs, next, and back button. working on input validations now

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,25 +5,22 @@ import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveryNew</h2>;
-
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
   render() {
     return (
-      <div className="container">
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route exact path="/surveys/new" component={SurveyNew} />
-          </div>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
